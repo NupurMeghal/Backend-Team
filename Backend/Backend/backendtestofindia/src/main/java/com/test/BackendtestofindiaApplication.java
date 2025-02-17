08151c4e319b29ac2ee120ae97d029e73be35a47
Nishant Agrawal <151541897+nishant9922@users.noreply.github.com>
2025-02-17 14:54:15 +0530

    Add files via upload

---
+++ Backend/Backend/backendtestofindia/src/main/java/com/test/BackendtestofindiaApplication.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtestofindiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendtestofindiaApplication.class, args);
+	}
+
+}
